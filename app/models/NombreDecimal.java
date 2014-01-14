42d97bdf9e57b475d39509362a5fe683ef0fe26f
Gatzby <jean-francois.carpentier@etu.univ-orleans.fr>
2014-01-14 10:27:48 +0100

    ajout2 de Jean-François

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -17,6 +17,9 @@ public class NombreDecimal {
 		switch(valeur){
 			case 1 : return "I";
 			case 2 : return "II";
+			case 3 : return "III";
+			case 4 : return "IV";
+			case 5 : return "V";
 			default:return "";
 		}
 	
