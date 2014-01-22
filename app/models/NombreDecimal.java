8d2a1d565e1bcb9f1eb19c60d28a53212cc39e27
Blain Florian <Dark.joosh@gmail.com>
2014-01-22 17:27:01 +0100

    Ajout des tests jusqu'a 10

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -11,16 +11,23 @@ public class NombreDecimal {
     public NombreDecimal(int n) {
 	this.valeur = n;
     }
+    public static String[] table_unites={"I","II","III","IV","V","VI","VII","VIII","IX","X"};
 
-    public String en_romain() {
-	if(valeur==1){
-	    return "I";
-	}
-	if(valeur==2){
-	    return "II";
-	}
-	else{
-	    return "";
-	}
-    }
+    /* public String en_romain() {
+	String[] chiffresRomain = new String [10];
+	
+	chiffresRomain[0]="";
+	chiffresRomain[1]="I";
+	chiffresRomain[2]="II";
+	chiffresRomain[3]="III";
+	chiffresRomain[4]="IV";
+	chiffresRomain[5]="V";
+
+	return chiffreRomain[this.valeur];
+	}*/
+
+    public String en_romain(int n){
+	return table_unites[n];
+
 }
+
